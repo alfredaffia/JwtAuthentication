c2e4b78004bebbd4bbfb30cf0967bc0f2eba6921
alfredaffia <alfredaffia478@gmail.com>
2025-04-08 16:58:44 +0100

    prettier

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
