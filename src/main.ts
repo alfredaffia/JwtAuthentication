88aaf88ba43b329405f2ff53c27d9f0ef2a7a0e9
alfredaffia <alfredaffia478@gmail.com>
2025-04-16 19:11:31 +0100

    server console

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 2000);
+  console.log(`server is running on port ${process.env.PORT ?? 2000}`)
 }
 bootstrap();
